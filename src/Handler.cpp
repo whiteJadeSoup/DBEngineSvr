814fd8f3106e0eb7297b42d4f6a1e46fb0a6ff9b
shubin <dzshubin@live.com>
2016-02-21 22:20:56 +0800

    异常处理更健壮

--- src/Handler.cpp
+++ src/Handler.cpp
@@ -31,7 +31,7 @@ Handler::~Handler()
 void Handler::read_head_from_socket()
 {
 
-    std::cout << "start read head info!" << std::endl;
+    cout << "start read head info!" << endl;
     auto self = shared_from_this();
     async_read(m_sock, boost::asio::buffer(head_info),
             [this, self] (const err_code& ec, size_t len)
@@ -39,16 +39,31 @@ void Handler::read_head_from_socket()
 
                 if (!ec)
                 {
-                    int nLen = get_len();
-                    int nType = get_type();
+                    try
+                    {
+                        int nLen = get_len();
+                        int nType = get_type();
 
-                    std::cout << "data len: "<< nLen << "msg type: "<< nType << std::endl;
-                    read_body_from_socket(nLen, nType);
+                        cout << "data len: "<< nLen << "msg type: "<< nType << endl;
+                        read_body_from_socket(nLen, nType);
+                    }
+                    catch (exception& e)
+                    {
+                        cout << "# ERR: exception in " << __FILE__;
+                        cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+                        cout << "# ERR: " << e.what() << endl;
+
+                        read_head_from_socket();
+                    }
                 }
 
                 else
                 {
-                    std::cout << "read head error: " <<ec.message() << std::endl;
+                    cout << "# ERR: exception in " << __FILE__;
+                    cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+                    cout << "# ERR: " << ec.message() << endl;
+
+                    m_sock.close();
                 }
             });
 }
@@ -62,13 +77,17 @@ void Handler::read_body_from_socket(int len_, int type_)
 
                 if(!ec)
                 {
-                    std::cout << "readed data size: " << len <<std::endl;
+                    cout << "readed data size: " << len <<endl;
                     process_msg(type_);
                 }
 
                 else
                 {
-                    std::cout << "read body error: " <<ec.message() << std::endl;
+                    cout << "# ERR: exception in " << __FILE__;
+                    cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+                    cout << "# ERR: " << ec.message() << endl;
+
+                    m_sock.close();
                 }
 
                 read_head_from_socket();
@@ -89,14 +108,14 @@ ip::tcp::socket& Handler::get_socket()
 
 int Handler::get_len()
 {
-    std::string len;
+    string len;
     copy(head_info.begin(), head_info.begin()+4, back_inserter(len));
     return std::stoi(len);
 }
 
 int Handler::get_type()
 {
-    std::string type;
+    string type;
     copy(head_info.begin()+4, head_info.end(), back_inserter(type));
     return std::stoi(type);
 }
@@ -104,43 +123,47 @@ int Handler::get_type()
 
 void Handler::encode_msg(CMsg& msg)
 {
-    std::cout << "start encode msg" << std::endl;
+    cout << "start encode msg" << endl;
     send_str.clear();
 
     char len[5];
-    sprintf(len, "%4d", msg.get_data_len());
+    sprintf(len, "%4d", msg.send_data_len());
 
     char type[5];
     sprintf(type, "%4d", msg.get_msg_type());
 
     // data
-    std::string data = msg.get_send_data();
+    string data = msg.get_send_data();
 
     send_str.append(len);
     send_str.append(type);
     send_str.append(data);
 
-    std::cout << "headinfo: "      << send_str.substr(0,8) << std::endl;
-    std::cout << "data size: "      << data.size()          << std::endl;
-    std::cout << "send str size: "      << send_str.size()      << std::endl;
+    cout << "headinfo: "      << send_str.substr(0,8) << endl;
+    cout << "data size: "      << data.size()          << endl;
+    cout << "send str size: "      << send_str.size()      << endl;
 }
 
 void Handler::send_msg(ip::tcp::socket& sock_, CMsg& msg)
 {
     encode_msg(msg);
 
-    std::cout << "start send msg." << std::endl;
+    cout << "start send msg." << endl;
     auto self = shared_from_this();
     async_write(sock_, boost::asio::buffer(send_str),
-            [this, self] (const err_code& ec, size_t len)
+            [this, self, &sock_] (const err_code& ec, size_t len)
             {
                 if (!ec)
                 {
-                    std::cout << "send data to client len: " << len << std::endl;
+                    cout << "send data to client len: " << len << endl;
                 }
                 else
                 {
-                    std::cout << "send msg error: " << ec.message() << std::endl;
+                    cout << "# ERR: exception in " << __FILE__;
+                    cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+                    cout << "# ERR: " << ec.message() << endl;
+
+                    sock_.close();
                 }
             });
 
@@ -150,18 +173,22 @@ void Handler::send_msg(CMsg& msg)
 {
     encode_msg(msg);
 
-    std::cout << "start send msg." << std::endl;
+    cout << "start send msg." << endl;
     auto self = shared_from_this();
     async_write(m_sock, boost::asio::buffer(send_str),
                 [this, self] (const err_code& ec, size_t len)
                 {
                     if (!ec)
                     {
-                        std::cout << "send data to client len: " << len << std::endl;
+                        cout << "send data to client len: " << len << endl;
                     }
                     else
                     {
-                        std::cout << "send msg error: " << ec.message() << std::endl;
+                        cout << "# ERR: exception in " << __FILE__;
+                        cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+                        cout << "# ERR: " << ec.message() << endl;
+
+                        m_sock.close();
                     }
 
                 });
