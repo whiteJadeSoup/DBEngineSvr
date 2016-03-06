69ffff03c9c390c2aef70ba63f3f9374bcf8b6df
shubin <dzshubin@live.com>
2016-03-06 17:12:51 +0800

    增加与msgsvr的通信

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,6 +1,11 @@
 
 #include "Server.hpp"
 
+
+#include "LoginHandler.hpp"
+#include "MsgHandler.hpp"
+
+
 #include <memory>
 #include <iostream>
 
@@ -11,12 +16,15 @@ using err_code = boost::system::error_code;
  *
  *
  */
-Server::Server(io_service& io_, unsigned short port_)
-  :m_LoginSock (io_),
-   m_LoginAcc (io_, ip::tcp::endpoint(ip::tcp::v4(), port_))
+Server::Server(io_service& io_, unsigned short port_, unsigned short msg_port_)
+  :m_LoginSock(io_),
+   m_LoginAcc(io_, ip::tcp::endpoint(ip::tcp::v4(), port_)),
+   m_MsgSock(io_),
+   m_MsgAcc(io_, ip::tcp::endpoint(ip::tcp::v4(), msg_port_))
 
 {
     wait_login_accept();
+    wait_msgsvr_accept();
 }
 
 
@@ -28,8 +36,7 @@ Server::Server(io_service& io_, unsigned short port_)
  */
 void Server::wait_login_accept()
 {
-    m_LoginAcc.async_accept(m_LoginSock,
-        [this] (const err_code& ec)
+    m_LoginAcc.async_accept(m_LoginSock, [this] (const err_code& ec)
         {
             if (!ec)
             {
@@ -39,8 +46,27 @@ void Server::wait_login_accept()
                      << m_LoginSock.remote_endpoint().port() << std::endl;
 
                 // read to start
-                std::make_shared<LoginHander>(std::move(m_LoginSock))->start();
+                std::make_shared<LoginHandler>(std::move(m_LoginSock))->start();
             }
             wait_login_accept();
         });
+}
+
+
+void Server::wait_msgsvr_accept()
+{
+    m_MsgAcc.async_accept(m_MsgSock, [this] (const err_code& ec)
+        {
+            if (!ec)
+            {
+                cout << "msgsvr address: "
+                     << m_MsgSock.remote_endpoint().address().to_string()
+                     << "msgsvr port: "
+                     << m_MsgSock.remote_endpoint().port() << endl;
+
+                make_shared<MsgHandler>(move(m_MsgSock))->start();
+            }
+            wait_msgsvr_accept();
+        });
+
 }
--- src/Server.hpp
+++ src/Server.hpp
@@ -4,21 +4,28 @@
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/io_service.hpp>
 
-#include "LoginHandler.hpp"
 using namespace boost::asio;
+using namespace std;
+
 
 class Server
 {
 public:
-    Server (io_service&, unsigned short);
+    Server (io_service&, unsigned short, unsigned short);
 
     void wait_login_accept();
+
+    void wait_msgsvr_accept();
 
 private:
     // socket for loginSvr
     ip::tcp::socket     m_LoginSock;
     // 监听loginSvr的连接
     ip::tcp::acceptor   m_LoginAcc;
+
+    ip::tcp::socket     m_MsgSock;
+
+    ip::tcp::acceptor   m_MsgAcc;
 };
 
 
