8ce30b90d18d05a9bd9c1b4bd8832e9274a82568
shubin <dzshubin@live.com>
2016-03-30 20:52:05 +0800

    mysql读取联系人信息

--- src/SQLhandler.cpp
+++ src/SQLhandler.cpp
@@ -99,3 +99,99 @@ bool SQLhandler::read_info(connection_ptr conn_, int64_t userid_, vector<string>
         return false;
     }
 }
+
+
+
+bool SQLhandler::read_contacts(connection_ptr conn_, int64_t user_id_, vector<string>& vPassData)
+{
+
+    try
+    {
+        if (!conn_->isValid())
+        {
+            cout << "conn is invalid!" << endl;
+            return false;
+        }
+
+        // 数据库
+        conn_->setSchema("account");
+
+
+        vector<string> contact_ids;
+        // 获得联系人列表
+        sql::PreparedStatement* prep_stmt =
+            conn_->prepareStatement("SELECT c_id FROM t_contacts WHERE u_id=?");
+
+        prep_stmt->setString(1, to_string(user_id_));
+        sql::ResultSet* res = prep_stmt->executeQuery();
+
+        while (res->next())
+        {
+            contact_ids.push_back(res->getString("c_id"));
+        }
+
+
+        // 获得联系人具体信息
+        prep_stmt = conn_->prepareStatement("SELECT name, nick_name FROM t_user_info WHERE id=?");
+
+        auto it = contact_ids.begin();
+        for(; it != contact_ids.end(); ++it)
+        {
+            prep_stmt->setString(1, *it);
+            res = prep_stmt->executeQuery();
+
+            if (res->next())
+            {
+                vPassData.push_back(*it);                               // id
+                vPassData.push_back(res->getString("name"));            // name
+                vPassData.push_back(res->getString("nick_name"));       // nick_name
+            }
+            else
+            {
+                cout << "No data: " << __FILE__ << endl;
+            }
+        }
+
+
+
+        delete prep_stmt;
+        delete res;
+
+        return true;
+
+    }
+    catch (sql::SQLException& e)
+    {
+        cout << "# ERR: SQLException in " << __FILE__;
+        cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+        cout << "# ERR: " << e.what();
+        cout << " (MySQL error code: " << e.getErrorCode();
+        cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
